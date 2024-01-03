65c6e1dc3543b9d2e00516564785c80d73adb53c
Itu <131593996+WAR-DEPT@users.noreply.github.com>
2024-01-03 17:12:10 +0530

    first commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2018-2020 The derpOS Project
+# Copyright (C) 2020 The PixelExperience Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/derp_violet.mk
+
+COMMON_LUNCH_CHOICES := \
+    derp_violet-eng \
+    derp_violet-userdebug \
+    derp_violet-user
